61a68876329023c80168a025c56f36224f5c2a37
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-12-01 22:26:49 -0300

    Adicionando a injeção de dependencia do DbContext no container de serviço da classe Program

--- BlazorShop.API/Program.cs
+++ BlazorShop.API/Program.cs
@@ -1,3 +1,6 @@
+using BlazorShop.API.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
 
 var app = builder.Build();
 
