98e82779d123649a17efe2b9e347b8f48df77a9d
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-12-03 07:24:54 -0300

    Adicionando as classes DTO na camada Models

---
+++ BlazorShop.Models/DTOs/CarrinhoItemAdicionaDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models.DTOs
+{
+    public class CarrinhoItemAdicionaDto
+    {
+        [Required]
+        public int CarrinhoId { get; set; }
+
+        [Required]
+        public int ProdutoId { get; set; }
+
+        [Required]
+        public int Quantidade { get; set; }
+    }
+}
---
+++ BlazorShop.Models/DTOs/CarrinhoItemAtualizaQuantidadeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorShop.Models.DTOs
+{
+    public class CarrinhoItemAtualizaQuantidadeDto
+    {
+        public int CarrinhoItemId { get; set; }
+        public int Quantidade { get; set; }
+    }
+}
