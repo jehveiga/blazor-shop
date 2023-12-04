48a40251b842ef72abd6597dfd7dff450651c456
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-12-04 19:41:32 -0300

    Adicionando a URL base na classe Program e imagem background da home da aplicação Blazor

--- BlazorShop.Web/Program.cs
+++ BlazorShop.Web/Program.cs
@@ -6,6 +6,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+string baseUrl = "https://localhost:7156";
+
+// Url base para serviço de acesso via HttpCliente, usando a injeção de dependencia do ASP .Net
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseUrl) });
 
 await builder.Build().RunAsync();
