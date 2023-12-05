16a0ace37d9ff0dddb30ad621cc0ec64c065dab7
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-12-04 21:39:18 -0300

    Adicionando a classe ProdutoService para consumir o serviço de HttpClient por injeção de dependencia

---
+++ BlazorShop.Web/Services/ProdutoService.cs
@@ -0,0 +1,33 @@
+﻿using BlazorShop.Models.DTOs;
+using System.Net.Http.Json;
+
+namespace BlazorShop.Web.Services
+{
+    public class ProdutoService : IProdutoService
+    {
+        private readonly HttpClient _httpClient;
+        private ILogger<ProdutoDto> _logger;
+
+        public ProdutoService(HttpClient httpClient, ILogger<ProdutoDto> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+        }
+
+        public async Task<IEnumerable<ProdutoDto>> GetItens()
+        {
+            try
+            {
+                var produtosDto = await _httpClient.GetFromJsonAsync<IEnumerable<ProdutoDto>>("api/produtos");
+
+                return produtosDto;
+            }
+            catch (Exception)
+            {
+                _logger.LogError("Erro ao acessar produtos : api/produtos ");
+                throw;
+            }
+
+        }
+    }
+}
