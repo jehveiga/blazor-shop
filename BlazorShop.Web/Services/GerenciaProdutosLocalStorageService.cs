b50889c44553f7819cf9e9e7fb4686822c041e26
Jefferson Veiga <gelsvsampa@hotmail.com>
2023-12-22 19:49:46 -0300

    Implementar a classe e interface de Gerenciar Coleção de produtos no local storage do cliente

---
+++ BlazorShop.Web/Services/GerenciaProdutosLocalStorageService.cs
@@ -0,0 +1,43 @@
+﻿using Blazored.LocalStorage;
+using BlazorShop.Models.DTOs;
+using BlazorShop.Web.Services.Interfaces;
+
+namespace BlazorShop.Web.Services
+{
+    public class GerenciaProdutosLocalStorageService : IGerenciaProdutosLocalStorageService
+    {
+        // chave que será usada para identificar a coleção que será adicionada, obtida ou excluida do Local Storage
+        private const string key = "ProdutoCollection";
+
+        private readonly ILocalStorageService _localStorageService;
+        private readonly IProdutoService _produtoService;
+
+        public GerenciaProdutosLocalStorageService(ILocalStorageService localStorageService, IProdutoService produtoService)
+        {
+            _localStorageService = localStorageService;
+            _produtoService = produtoService;
+        }
+
+        // Tentará obter a lista de produtos no local storage se não conseguir Efetuará um Get do serviço
+        // e usará o método Set para adicionar os itens no local storage
+        public async Task<IEnumerable<ProdutoDto>> GetCollection()
+        {
+            return await _localStorageService.GetItemAsync<IEnumerable<ProdutoDto>>(key)
+                            ?? await AddCollection();
+        }
+
+        public async Task RemoveCollection()
+        {
+            await _localStorageService.RemoveItemAsync(key);
+        }
+
+        private async Task<IEnumerable<ProdutoDto>> AddCollection()
+        {
+            var produtosCollection = await _produtoService.GetItens();
+            if (produtosCollection != null)
+                await _localStorageService.SetItemAsync(key, produtosCollection);
+
+            return produtosCollection;
+        }
+    }
+}
---
+++ BlazorShop.Web/Services/Interfaces/IGerenciaProdutosLocalStorageService.cs
@@ -0,0 +1,10 @@
+﻿using BlazorShop.Models.DTOs;
+
+namespace BlazorShop.Web.Services.Interfaces
+{
+    public interface IGerenciaProdutosLocalStorageService
+    {
+        Task<IEnumerable<ProdutoDto>> GetCollection();
+        Task RemoveCollection();
+    }
+}
